93ccf55d3322b32183342c056cafadd09038df77
Kamil <kamil.lobasiuk@gmailc.om>
2021-07-08 08:20:43 +0200

    Start of the project Added dependencies: * Actuator * Spring data JPA * Spring web * Swagger * Prometheus * H2 * Lombok

---
+++ src/test/java/com/example/sw_api/SwApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sw_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
